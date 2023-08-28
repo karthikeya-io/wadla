97d97e57c08e99fd1b25e8cd14f546f058389529
k charan kumar <97822008+charan1409@users.noreply.github.com>
2023-08-28 23:26:39 +0530

    login page frontend done (#3) closes (#2)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import RegistrationPage from "./pages/RegistrationPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="register" element={<RegistrationPage />} />
+        <Route path="login" element={<LoginPage />} />
         {/* <Route path="about" element={<h1>About</h1>} /> */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
