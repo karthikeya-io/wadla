164257a53c18f8820cdaa7567bd0c8055e307aa0
Kamal sai Yalamarthi <98586894+kamalsai1@users.noreply.github.com>
2023-09-24 23:03:13 +0530

    recordings page done! (#7)

---
+++ src/EventList.js
@@ -0,0 +1,31 @@
+export const EventList = [
+    {
+        id: 1,
+        title: 'Day 1',
+        days:[
+            {id: 1,name: "Event 1",videoId: "https://www.youtube.com/watch?v=EYxrDT6ThKc&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=1&pp=iAQB" },
+            {id: 2,name: "Event 2",videoId: "https://www.youtube.com/watch?v=4XXJjBAWW_8&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=2&pp=iAQB" },
+            {id: 3,name: "Event 3",videoId: "https://www.youtube.com/watch?v=LbTnEo0Wu3M&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=3&pp=iAQB" },
+        ]
+    },
+    {
+        id: 2,
+        title: 'Day 2',
+        days:[
+            {id: 1,name: "Event 1",videoId: "https://www.youtube.com/watch?v=EYxrDT6ThKc&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=1&pp=iAQB" },
+            {id: 2,name: "Event 2",videoId: "https://www.youtube.com/watch?v=4XXJjBAWW_8&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=2&pp=iAQB" },
+            {id: 3,name: "Event 3",videoId: "https://www.youtube.com/watch?v=LbTnEo0Wu3M&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=3&pp=iAQB" },
+        ]
+    },
+    {
+        id: 3,
+        title: 'Day 3',
+        days:[
+            {id: 1,name: "Event 1",videoId: "https://www.youtube.com/watch?v=EYxrDT6ThKc&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=1&pp=iAQB" },
+            {id: 2,name: "Event 2",videoId: "https://www.youtube.com/watch?v=4XXJjBAWW_8&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=2&pp=iAQB" },
+            {id: 3,name: "Event 3",videoId: "https://www.youtube.com/watch?v=LbTnEo0Wu3M&list=PLFVY9UMYveR47NPTDEAyJFAeYhswCwJ3T&index=3&pp=iAQB" },
+        ]
+    }
+]
+
+//"G2fqAlgmoPo"
