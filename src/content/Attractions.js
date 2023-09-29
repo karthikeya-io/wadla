62d78606813dbb12d49d5c2087d93b6d6a9c278a
charan14092003 <charankallempudi24@gmail.com>
2023-09-30 00:30:29 +0530

    nearest attractions page done

---
+++ src/content/Attractions.js
@@ -0,0 +1,57 @@
+import Pulicat_Lake from "../assets/pulicat_lake.webp";
+import Tirumala from "../assets/Tirumala.webp";
+import Sriharikota from "../assets/sriharikota.webp";
+import Srikalahastini from "../assets/srikalahasthi.webp";
+import Tada_Falls from "../assets/tada falls.webp";
+import Bird_Sanctuary from "../assets/bird_sanctuary.webp";
+
+export const Attractions = [
+  {
+    id: 1,
+    title: "Pulicat Lake",
+    image: Pulicat_Lake,
+    description:
+      "Pulicat Lake is the second largest brackish water lake or lagoon in India, after Chilika Lake. It straddles the border of Andhra Pradesh and Tamil Nadu states on the Coromandal Coast in South India. The lake encompasses the Pulicat Lake Bird Sanctuary. The barrier island of Sriharikota separates the lake from the Bay of Bengal and is home to the Satish Dhawan Space Centre.",
+    location: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d124116.60047177295!2d80.0358540042165!3d13.557431383870831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x3a4d773f1e0f8721%3A0xadb0842ffc2719e4!2sIndian%20Institute%20of%20Information%20Technology%2C%20Sri%20City%2C%20Chittoor%2C%20Sathyavedu%2C%20Andhra%20Pradesh!3m2!1d13.5552932!2d80.02669!4m5!1s0x3a4d9ca00b6b7437%3A0x112d5630394c8e76!2sPulicat%20Lake!3m2!1d13.559262799999999!2d80.2098219!5e0!3m2!1sen!2sin!4v1696003073820!5m2!1sen!2sin"
+  },
+  {
+    id: 2,
+    title: "Tirumala",
+    image: Tirumala,
+    description:
+      "Tirupati is a hill town where Tirumala Venkateswara Temple is located, which is the abode of Lord Venkateswara. Tirumala is located 3,200 feet (980 m) above sea level and covers an area of approximately 10.33 square miles (26.8 km2). Surrounding the hills are seven peaks of Seshachalam range, Eastern Ghats namely Seshadri, Neeladri, Garudadri, Anjanadri, Vrushabadri, Narayanadri and Venkatadri. The temple of Sri Venkateswara is on the seventh peak (Venkatadri). The temple is built in Dravidian architecture and is believed to be constructed over a period of time starting from 300 AD.",
+    location:"https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d496335.9361371877!2d79.3577075027882!3d13.619730744529614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x3a4d773f1e0f8721%3A0xadb0842ffc2719e4!2sIndian%20Institute%20of%20Information%20Technology%2C%20Sri%20City%2C%20Chittoor%2C%20Sathyavedu%2C%20Andhra%20Pradesh!3m2!1d13.5552932!2d80.02669!4m5!1s0x3a4d4b19d3f65f0b%3A0x9ceb617526255701!2sTirupati%20Temple%2C%20Tirumala%2C%20Tirupati%2C%20Andhra%20Pradesh!3m2!1d13.6832674!2d79.3480505!5e0!3m2!1sen!2sin!4v1696010469839!5m2!1sen!2sin"
+  },
+  {
+    id: 3,
+    title: "Sriharikota",
+    image: Sriharikota,
+    description:
+      "Sriharikota is a barrier island off the Bay of Bengal coast located in the Indian state of Andhra Pradesh, India. It houses the Satish Dhawan Space Centre, one of the two satellite launch centres in India with the other being the Thumba Equatorial Rocket Launching Station in Thiruvananthapuram.",
+      location:"https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d248145.7787300211!2d79.96180929713447!3d13.640858599258369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x3a4d773f1e0f8721%3A0xadb0842ffc2719e4!2sIndian%20Institute%20of%20Information%20Technology%2C%20Sri%20City%2C%20Chittoor%2C%20Sathyavedu%2C%20Andhra%20Pradesh!3m2!1d13.5552932!2d80.02669!4m5!1s0x3a4da27eef264eaf%3A0xe9dec0ba1d50651f!2sSatish%20Dhawan%20Space%20Centre%2C%20Sriharikota%2C%20Andhra%20Pradesh!3m2!1d13.725865299999999!2d80.22655449999999!5e0!3m2!1sen!2sin!4v1696010569490!5m2!1sen!2sin"
+  },
+  {
+    id: 4,
+    title: "Srikalahasti",
+    image: Srikalahastini,
+    description:
+      "Srikalahasti is a holy town in Chittoor district of the Indian state of Andhra Pradesh. It is a municipality and the mandal headquarters of Srikalahasti mandal in Tirupati revenue division. The town is a part of Tirupati Urban Development Authority, which is located on the banks of the River Swarnamukhi. It is one of the ancient temples included in the Saiva pilgrimages and is often listed in many Hindu scriptures.",
+      location:"https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d248133.4206523512!2d79.69766064787882!3d13.652611589428302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x3a4d773f1e0f8721%3A0xadb0842ffc2719e4!2sIndian%20Institute%20of%20Information%20Technology%2C%20Sri%20City%2C%20Chittoor%2C%20Sathyavedu%2C%20Andhra%20Pradesh!3m2!1d13.5552932!2d80.02669!4m5!1s0x3a4d3e54318a70e9%3A0x706957a2638f13c1!2sSrikalahasti%20Temple%2C%20Srikalahasti%2C%20Andhra%20Pradesh!3m2!1d13.7493714!2d79.6982572!5e0!3m2!1sen!2sin!4v1696010632449!5m2!1sen!2sin"
+  },
+  {
+    id: 5,
+    title: "Tada Falls",
+    image: Tada_Falls,
+    description:
+      "Tada Falls is a waterfall located in Chittoor district of the Indian state of Andhra Pradesh. It is situated at a distance of 90 kilometres (56 mi) from Chennai and 65 kilometres (40 mi) from Tirupati. The falls are located within the Kambakam hills and are a popular destination for trekking.",
+      location:"https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d124104.86609459098!2d79.85284815455408!3d13.579877047434492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x3a4d773f1e0f8721%3A0xadb0842ffc2719e4!2sIndian%20Institute%20of%20Information%20Technology%2C%20Sri%20City%2C%20Chittoor%2C%20Sathyavedu%2C%20Andhra%20Pradesh!3m2!1d13.5552932!2d80.02669!4m5!1s0x3a4d6ea4fdda9ce1%3A0xb64f97329717627e!2sTada%20Waterfalls%2C%20Andhra%20Pradesh!3m2!1d13.6041542!2d79.8438102!5e0!3m2!1sen!2sin!4v1696010697026!5m2!1sen!2sin"
+  },
+  {
+    id: 6,
+    title: "Nelapattu Bird Sanctuary",
+    image: Bird_Sanctuary,
+    description:
+      "Nelapattu Bird Sanctuary is a bird sanctuary in Nellore district, Andhra Pradesh, India, near the village of Nelapattu. It has an area of 458.92 hectares. It is an important breeding site for spot-billed pelicans. The sanctuary is located on the Chennai-Kolkata highway, about 20 kilometres (12 mi) north of the Pulicat Lake. The nearest town is Sullurpeta, which is about 20 kilometres (12 mi) away. The sanctuary is about 80 kilometres (50 mi) from Chennai and 50 kilometres (31 mi) from Tirupati. The sanctuary is open from 8:00 am to 5:00 pm. The best time to visit is from October to March.",
+      location:"https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d248093.56115977617!2d79.82331775028094!3d13.69045199676382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x3a4d773f1e0f8721%3A0xadb0842ffc2719e4!2sIndian%20Institute%20of%20Information%20Technology%2C%20Sri%20City%2C%20Chittoor%2C%20Sathyavedu%2C%20Andhra%20Pradesh!3m2!1d13.5552932!2d80.02669!4m5!1s0x3a4d0c45b4d70b19%3A0xef9f12afbb479026!2sNelapattu%20Bird%20Sanctuary%2C%20Nelapattu%2C%20Andhra%20Pradesh!3m2!1d13.8250526!2d79.9495714!5e0!3m2!1sen!2sin!4v1696010772938!5m2!1sen!2sin"
+  }
+];
